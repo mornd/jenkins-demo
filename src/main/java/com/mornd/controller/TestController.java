a44fbea5725499155ba64f44de127039eaf9c7ae
yanglinchen <1152229579@qq.com>
2026-06-20 12:17:42 +0800

    version upgrade

--- src/main/java/com/mornd/controller/TestController.java
+++ src/main/java/com/mornd/controller/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    private final String VERSION = "v1.0";
+    private final String VERSION = "v1.1";
 
     @GetMapping("/version")
     public Object version() {
