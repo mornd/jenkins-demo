85fe64f18bebe25e290a385fee8b7f648caf018a
yanglinchen <1152229579@qq.com>
2026-06-20 12:14:19 +0800

    init

---
+++ src/main/java/com/mornd/App.java
@@ -0,0 +1,16 @@
+package com.mornd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mornd
+ * @date 2026/6/20 12:07
+ */
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/mornd/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.mornd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mornd
+ * @date 2026/6/20 12:08
+ */
+
+@RestController
+public class TestController {
+    private final String VERSION = "v1.0";
+
+    @GetMapping("/version")
+    public Object version() {
+        return VERSION;
+    }
+}
